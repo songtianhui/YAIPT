57ae83009b8542282500e5f5e306ee5e4e5bf0a5
song tianhui <191098194@smail.nju.edu.cn>
2021-10-05 12:06:00 +0800

    plane implemented basely

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,20 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy import ndarray
 
 
 def plane(img: ndarray) -> ndarray:
-    return None
+    assert img.ndim == 2
+    planes = []
+    mask = 0b1 << 7
+    for i in range(9):
+        planes.append(np.left_shift(np.bitwise_and(img, mask), i))
+        mask = mask >> 1
+    layer1 = np.hstack(planes[0:3])
+    layer2 = np.hstack(planes[3:6])
+    layer3 = np.hstack(planes[6:9])
+    res = np.vstack((layer1, layer2, layer3))
+    return res
 
 
 def equalize(img: ndarray) -> ndarray:
