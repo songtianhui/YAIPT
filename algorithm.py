c03a1f49d2813d1288786c01e38493bc9965ad56
song tianhui <191098194@smail.nju.edu.cn>
2021-10-24 19:56:40 +0800

    fft completed easily

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+# import cv2
+# import scipy.ndimage
 from numpy import ndarray
+
+def merge(img, func_channel, *args):
+    assert img.ndim == 3
+    if img.ndim == 3:
+        r = img[:,:,0]
+        g = img[:,:,1]
+        b = img[:,:,2]
+        r_out = func_channel(r)
+        g_out = func_channel(g)
+        b_out = func_channel(b)
+    return np.dstack((r_out,g_out,b_out))
 
 
 def plane(img: ndarray) -> ndarray:
@@ -16,6 +29,7 @@ def plane(img: ndarray) -> ndarray:
     res = np.vstack((layer1, layer2, layer3))
     return res
 
+
 def equalize_channel(image, number_bins=256):
     # get image histogram
     image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
@@ -29,27 +43,57 @@ def equalize_channel(image, number_bins=256):
 
 def equalize(img: ndarray) -> ndarray:
     if img.ndim == 3:
-        r = img[:,:,0]
-        g = img[:,:,1]
-        b = img[:,:,2]
-        r_out = equalize_channel(r)
-        g_out = equalize_channel(g)
-        b_out = equalize_channel(b)
-        return np.dstack((r_out,g_out,b_out))
+        return merge(img, equalize_channel)
     else:
         return equalize_channel(img)
 
 
-def denoise(img: ndarray) -> ndarray:
-    return None
+def denoise_channel(img):
+    pad_num = 1
+    image_pad = img.copy()
+    image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
+    w, h = image_pad.shape
+    out = image_pad.copy()
 
+    for i in range(pad_num, w - pad_num):
+        for j in range(pad_num, h - pad_num):
+            out[i, j] = np.median(image_pad[i - pad_num:i + pad_num + 1, j - pad_num:j + pad_num + 1])
+
+    out = out[pad_num:w - pad_num, pad_num:h - pad_num]
+    return out
+
+# def denoise_scipy(img):
+#     img = scipy.ndimage.median_filter(img, (3, 3))
+#     return img
+
+# def denoise_opencv(img):
+#     img = np.multiply(img, 255).astype(np.uint8)
+#     img = cv2.medianBlur(img, 3)
+#     return np.divide(img.astype(np.float32), 255)
+
+def denoise(img: ndarray) -> ndarray:
+    if img.ndim == 3:
+        return merge(img, denoise_channel)
+    else:
+        return denoise_channel(img)
+
+
+def interpolate_channel(img: ndarray):
+  nR0 = len(img)     # source number of rows
+  nC0 = len(img[0])  # source number of columns
+  nR = img.shape[0] * 2
+  nC = img.shape[1] * 2
+  return np.array([[ img[int(nR0 * r / nR)][int(nC0 * c / nC)]  for c in range(nC)] for r in range(nR)])
 
 def interpolate(img: ndarray) -> ndarray:
-    return None
-
+    if img.ndim == 3:
+        return merge(img, interpolate_channel)
+    else:
+        return interpolate_channel(img)
 
 def dft(img: ndarray) -> ndarray:
-    return None
+    print(img.ndim)
+    return np.log(np.abs(np.fft.fftshift(np.fft.fft2(img))))
 
 
 def butterworth(img: ndarray) -> ndarray:
