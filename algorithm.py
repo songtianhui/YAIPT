1db95fccd6b6bc89f138cb385274690eb3f0e83f
song tianhui <191098194@smail.nju.edu.cn>
2021-11-10 18:41:36 +0800

    canny complete base;y

--- algorithm.py
+++ algorithm.py
@@ -29,63 +29,56 @@ def plane(img: ndarray) -> ndarray:
     res = np.vstack((layer1, layer2, layer3))
     return res
 
-
-def equalize_channel(image, number_bins=256):
-    # get image histogram
-    image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
-    cdf = image_histogram.cumsum() # cumulative distribution function
-    cdf = cdf / cdf[-1] # normalize
-
-    # use linear interpolation of cdf to find new pixel values
-    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
-
-    return image_equalized.reshape(image.shape)
-
 def equalize(img: ndarray) -> ndarray:
+    def equalize_channel(image, number_bins=256):
+        # get image histogram
+        image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+        cdf = image_histogram.cumsum() # cumulative distribution function
+        cdf = cdf / cdf[-1] # normalize
+
+        # use linear interpolation of cdf to find new pixel values
+        image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+        return image_equalized.reshape(image.shape)
+
+
     if img.ndim == 3:
         return merge(img, equalize_channel)
     else:
         return equalize_channel(img)
 
-
-def denoise_channel(img):
-    pad_num = 1
-    image_pad = img.copy()
-    image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
-    w, h = image_pad.shape
-    out = image_pad.copy()
-
-    for i in range(pad_num, w - pad_num):
-        for j in range(pad_num, h - pad_num):
-            out[i, j] = np.median(image_pad[i - pad_num:i + pad_num + 1, j - pad_num:j + pad_num + 1])
-
-    out = out[pad_num:w - pad_num, pad_num:h - pad_num]
-    return out
-
-# def denoise_scipy(img):
-#     img = scipy.ndimage.median_filter(img, (3, 3))
-#     return img
-
-# def denoise_opencv(img):
-#     img = np.multiply(img, 255).astype(np.uint8)
-#     img = cv2.medianBlur(img, 3)
-#     return np.divide(img.astype(np.float32), 255)
-
 def denoise(img: ndarray) -> ndarray:
+    def gauss_denoise_channel(img):
+        gauss_filter = (np.array([1,2,1,2,4,2,1,2,1]) / 16).reshape((3,3))
+
+        pad_num = 1
+        image_pad = img.copy()
+        image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
+        w, h = image_pad.shape
+        out = image_pad.copy()
+
+        for i in range(pad_num, w - pad_num):
+            for j in range(pad_num, h - pad_num):
+                out[i, j] = np.multiply(image_pad[i - pad_num:i + pad_num + 1, j - pad_num:j + pad_num + 1], gauss_filter).sum()
+
+        out = out[pad_num:w - pad_num, pad_num:h - pad_num]
+        return out
+
+
     if img.ndim == 3:
-        return merge(img, denoise_channel)
+        return merge(img, gauss_denoise_channel)
     else:
-        return denoise_channel(img)
-
-
-def interpolate_channel(img: ndarray):
-  nR0 = len(img)     # source number of rows
-  nC0 = len(img[0])  # source number of columns
-  nR = img.shape[0] * 2
-  nC = img.shape[1] * 2
-  return np.array([[ img[int(nR0 * r / nR)][int(nC0 * c / nC)]  for c in range(nC)] for r in range(nR)])
+        return gauss_denoise_channel(img)
 
 def interpolate(img: ndarray) -> ndarray:
+    def interpolate_channel(img: ndarray):
+        nR0 = len(img)     # source number of rows
+        nC0 = len(img[0])  # source number of columns
+        nR = img.shape[0] * 2
+        nC = img.shape[1] * 2
+        return np.array([[ img[int(nR0 * r / nR)][int(nC0 * c / nC)]  for c in range(nC)] for r in range(nR)])
+
+
     if img.ndim == 3:
         return merge(img, interpolate_channel)
     else:
@@ -118,8 +111,82 @@ def butterworth(img: ndarray) -> ndarray:
     # return np.log(np.abs(G))
     return np.abs(np.fft.ifft2(np.fft.ifftshift(G)))
 
+
 def canny(img: ndarray) -> ndarray:
-    return None
+    def gauss(img):
+        return denoise(img)
+
+    def gradient(img):
+        sobely = np.array([-1, -2, -1, 0, 0, 0, 1, 2, 1]).reshape((3,3))
+        sobelx = sobely.transpose()
+        pad_num = 1
+        image_pad = img.copy()
+        image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
+        w, h = image_pad.shape
+        grad_x = image_pad.copy()
+        grad_y = image_pad.copy()
+
+        for i in range(pad_num, w - pad_num):
+            for j in range(pad_num, h - pad_num):
+                grad_x[i, j] = np.multiply(sobelx, image_pad[i - pad_num:i + pad_num + 1, j - pad_num:j + pad_num + 1]).sum()
+                grad_y[i, j] = np.multiply(sobely, image_pad[i - pad_num:i + pad_num + 1, j - pad_num:j + pad_num + 1]).sum()
+
+        grad_x = grad_x[pad_num:w - pad_num, pad_num:h - pad_num] + 0.00000001 # avoid div by zero
+        grad_y = grad_y[pad_num:w - pad_num, pad_num:h - pad_num]
+        
+        # m = np.sqrt(dx**2 + dy**2)
+        m = np.abs(grad_x) + np.abs(grad_y)
+        theta = np.arctan(grad_y / grad_x)
+        return m, theta
+
+    def non_maximum_supress(grad, theta):
+        theta = theta + np.pi / 2
+        pad_num = 1
+        image_pad = grad.copy()
+        image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
+        w, h = image_pad.shape
+        out = image_pad.copy()
+
+        for i in range(pad_num, w - pad_num):
+            for j in range(pad_num, h - pad_num):
+                if (theta[i-pad_num, j-pad_num] >= 0 and theta[i-pad_num, j-pad_num] < np.pi / 8) or (theta[i-pad_num, j-pad_num] >= 7 * np.pi / 8 and theta[i-pad_num, j-pad_num] < np.pi):
+                    out[i, j] = image_pad[i, j] if image_pad[i, j] >= image_pad[i-1, j] and image_pad[i, j] >= image_pad[i+1, j] else 0
+                elif (theta[i-pad_num, j-pad_num] >= np.pi / 8 and theta[i-pad_num, j-pad_num] < 3 * np.pi / 8):
+                    out[i, j] = image_pad[i, j] if image_pad[i, j] >= image_pad[i+1, j-1] and image_pad[i, j] >= image_pad[i-1, j+1] else 0
+                elif (theta[i-pad_num, j-pad_num] >= 3 * np.pi / 8 and theta[i-pad_num, j-pad_num] < 5 * np.pi / 8):
+                    out[i, j] = image_pad[i, j] if image_pad[i, j] >= image_pad[i, j-1] and image_pad[i, j] >= image_pad[i, j+1] else 0
+                else:
+                    out[i, j] = image_pad[i, j] if image_pad[i, j] >= image_pad[i+1, j+1] and image_pad[i, j] >= image_pad[i-1, j-1] else 0
+
+        out = out[pad_num:w - pad_num, pad_num:h - pad_num]
+        return out
+
+    def double_threshold(grad):
+        ht = 0.3
+        lt = 0.1
+        pad_num = 1
+        image_pad = grad.copy()
+        image_pad = np.pad(image_pad, (pad_num, pad_num), mode='constant')
+        w, h = image_pad.shape
+        out = image_pad.copy()
+
+        for i in range(pad_num, w - pad_num):
+            for j in range(pad_num, h - pad_num):
+                if image_pad[i, j] >= ht:
+                    out[i, j] = 1
+                elif image_pad[i, j] < lt:
+                    out[i, j] = 0
+                else:
+                    out[i, j] = 1 if (np.array([image_pad[i-pad_num, j], image_pad[i+pad_num, j], image_pad[i, j+pad_num], image_pad[i, j-pad_num]]) >= ht).any() else 0
+        
+        out = out[pad_num:w - pad_num, pad_num:h - pad_num]
+        return out
+
+    img_denoise = gauss(img)
+    grad, theta = gradient(img_denoise)
+    grad = non_maximum_supress(grad, theta)
+    grad = double_threshold(grad)
+    return grad
 
 
 def morphology(img: ndarray) -> ndarray:
